4a6d7c0bd6aa006666d9bea485d5ca6e6c4cee6e
mshyam001 <33834296+mshyam001@users.noreply.github.com>
2025-10-30 16:55:28 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,8 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
+  tt_um_VHDL_PWM_DEMUX user_project (
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
