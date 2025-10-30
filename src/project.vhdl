055142849b45e1fdd4c74b1025f4050f6a6f248a
mshyam001 <33834296+mshyam001@users.noreply.github.com>
2025-10-30 17:43:58 +0100

    Update project.vhdl

--- src/project.vhdl
+++ src/project.vhdl
@@ -16,53 +16,53 @@ entity tt_um_VHDL_PWM_DEMUX is
 end tt_um_VHDL_PWM_DEMUX;
 
 architecture rtl of tt_um_VHDL_PWM_DEMUX is
-  component pwm_cfg_demux is   
-            Port (
-                clk       : in  std_logic;
-                res_ni    : in  std_logic;
+  component pwm_cfg_demux is
+    port (
+      clk       : in  std_logic;
+      res_ni    : in  std_logic;
+      data_i    : in  unsigned(9 downto 0);           -- 10-bit shared bus
+      sel_i     : in  std_logic_vector(1 downto 0);   -- 00=set, 01=clear, 10=reload
+      wr_i      : in  std_logic;                      -- 1-cycle write strobe
+      commit_i  : in  std_logic;                      -- atomic apply
+      pwm_o     : out std_logic
+    );
+  end component;
 
-                -- Compact config interface (demuxed 8-bit bus)
-                data_i    : in  unsigned(7 downto 0);              -- shared 8-bit bus
-                sel_i     : in  std_logic_vector(1 downto 0);      -- "00"=set, "01"=clear, "10"=reload
-                wr_i      : in  std_logic;                         -- 1-cycle write strobe
-                commit_i  : in  std_logic;                         -- atomic apply (same-cycle OK)
-
-                pwm_o     : out std_logic
-                );
- end component;
-    
-    signal data_s      : unsigned(7 downto 0);
-    signal sel_s       : std_logic_vector(1 downto 0);
-    signal wr_s        : std_logic;
-    signal commit_s    : std_logic;
-    signal pwm_s       : std_logic;
-    signal res_ni_g    : std_logic;
+  signal data_s   : unsigned(9 downto 0);
+  signal sel_s    : std_logic_vector(1 downto 0);
+  signal wr_s     : std_logic;
+  signal commit_s : std_logic;
+  signal pwm_s    : std_logic;
+  signal res_ni_g : std_logic;
 begin
-    -- Hold in reset when not enabled (saves power on the shuttle)
-    res_ni_g <= rst_n and ena;
+  -- Gate reset with ena (saves power on shuttle)
+  res_ni_g <= rst_n and ena;
 
-    -- Use uio as inputs for control; don't drive them
-    uio_out <= (others => '0');
-    uio_oe  <= (others => '0');
+  -- uio are inputs only in this design
+  uio_out <= (others => '0');
+  uio_oe  <= (others => '0');
 
-    -- Map compact config interface
-    data_s   <= unsigned(ui_in);
-    sel_s    <= uio_in(1 downto 0);
-    wr_s     <= uio_in(2);
-    commit_s <= uio_in(3);
+  -- 10-bit config bus: MSBs on uio_in(5:4), LSBs on ui_in(7:0)
+  data_s <= unsigned(uio_in(5 downto 4) & ui_in);
 
-    -- Show PWM on uo_out(0); others low
-    uo_out <= "0000000" & pwm_s;
+  -- Controls
+  sel_s    <= uio_in(1 downto 0);
+  wr_s     <= uio_in(2);
+  commit_s <= uio_in(3);
 
-    -- Instantiate your wrapper+core
-    u_cfg: pwm_cfg_demux
-        port map (
-            clk       => clk,
-            res_ni    => res_ni_g,
-            data_i    => data_s,
-            sel_i     => sel_s,
-            wr_i      => wr_s,
-            commit_i  => commit_s,
-            pwm_o     => pwm_s
-        );
+  -- PWM on uo_out(0), others 0
+  uo_out <= (7 downto 1 => '0', 0 => pwm_s);
+
+  -- Instantiate 10-bit cfg+PWM
+  u_cfg: pwm_cfg_demux
+    port map (
+      clk       => clk,
+      res_ni    => res_ni_g,
+      data_i    => data_s,
+      sel_i     => sel_s,
+      wr_i      => wr_s,
+      commit_i  => commit_s,
+      pwm_o     => pwm_s
+    );
 end rtl;
+
